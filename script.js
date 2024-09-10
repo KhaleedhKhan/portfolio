2d13dae8d65411563b59c0e0a12f11f9be4bcc19
KhaleedhKhan <pathankhaleedhkhan.gmail.com>
2024-09-10 22:06:06 +0530

    Project-Updated

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ window.addEventListener("scroll", () => {
 
 function openCV() {
   // Replace 'path/to/cv.pdf' with the actual path to your CV file
-  var cvPath = 'CV/Pathan Khaleedh Khan.pdf';
+  var cvPath = 'CV/KK_RESUME.pdf';
 
   // Open the CV in a new tab
   window.open(cvPath, '_blank');
